0aa6775a91193be6cc0327835e59542b85f9f369
skatld123 <skatld123@naver.com>
2024-07-04 09:32:02 +0900

    update programmers_study

---
+++ Study/공원산책.py
@@ -0,0 +1,32 @@
+from collections import deque
+def solution(park, routes):
+    answer = []
+    sx, sy = 0, 0
+    for i in range(len(park)):
+        if 'S' in park[i]:
+            sx = i
+            sy = park[i].index('S')
+            
+    queue = deque(routes)
+    while queue:
+        direction, move = queue.popleft().split()
+        isWall, isOut = False, False
+        nx, ny = sx, sy
+        for i in range(int(move)):
+            if direction == "N":
+                nx -= 1
+            elif direction == "S":
+                nx += 1
+            elif direction == "W":
+                ny -= 1
+            elif direction == "E":
+                ny += 1
+            if not (0 <= nx < len(park) and 0 <= ny < len(park[0])):
+                isOut = True
+                break
+            if park[nx][ny] == 'X':
+                isWall = True
+                break
+        if not isOut and not isWall:
+            sx, sy = nx, ny
+    return [sx, sy]
---
+++ Study/등굣길_복기필요.py
@@ -0,0 +1,28 @@
+def solution(m, n, puddles):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(len(dp)):
+        if not (i, 0) in puddles: 
+            dp[i][0] = 1
+        else:
+            break
+            
+    for i in range(len(dp[0])):
+        if not (0, i) in puddles: 
+            dp[0][i] = 1
+        else:
+            break
+            
+    for x, y in puddles:
+        dp[x-1][y-1] = -1
+    
+    for i in range(len(dp)):
+        for j in range(len(dp[0])):
+            if dp[i][j] == 0 and i - 1 >= 0 and j - 1 >= 0:
+                if dp[i - 1][j] == -1:
+                    dp[i][j] = dp[i][j - 1]
+                elif dp[i][j - 1] == -1:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                
+    return dp[n - 1][m - 1]
